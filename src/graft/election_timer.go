a485cd28df7f5e6ac6fa48fcf9e326a3ecc91af7
benmills and wjdix <ben@benmills.org>
2013-08-23 20:54:31 +0000

    Add ElectionTimer.

---
+++ src/graft/election_timer.go
@@ -0,0 +1,56 @@
+package graft
+
+import (
+	"time"
+)
+
+type Electable interface {
+	StartElection()
+}
+
+type ElectionTimer struct {
+	electable Electable
+	ElectionChannel chan int
+	shutDownChannel chan int
+	resets int
+	duration time.Duration
+}
+
+func NewElectionTimer(duration time.Duration, electable Electable) *ElectionTimer {
+	timer := &ElectionTimer{
+		electable: electable,
+		ElectionChannel: make(chan int),
+		shutDownChannel: make(chan int),
+		duration: duration,
+	}
+
+	go timer.waitForElection()
+	return timer
+}
+
+func (timer *ElectionTimer) StartTimer() {
+	go func() {
+		time.Sleep(timer.duration)
+		timer.ElectionChannel <- 1
+		timer.StartTimer()
+	}()
+}
+
+func (timer *ElectionTimer) ShutDown() {
+	timer.shutDownChannel <- 1
+}
+
+func (timer *ElectionTimer) startElection() {
+	timer.electable.StartElection()
+}
+
+func (timer *ElectionTimer) waitForElection() {
+	for {
+		select {
+		case <- timer.ElectionChannel:
+			timer.startElection()
+		case <- timer.shutDownChannel:
+			return
+		}
+	}
+}
---
+++ src/graft/election_timer_test.go
@@ -0,0 +1,53 @@
+package graft
+
+import (
+	"github.com/benmills/quiz"
+	"testing"
+	"time"
+)
+
+type SpyServer struct {
+	electionStarted bool
+	electionCount int
+}
+
+func (server *SpyServer) StartElection() {
+	server.electionStarted = true
+	server.electionCount += 1
+}
+
+func TestTimerTellsServerToStartElectionWhenReceivingOnTimeoutChannel(t *testing.T) {
+	test := quiz.Test(t)
+
+	spyServer := &SpyServer{electionStarted: false, electionCount: 0}
+	timer := NewElectionTimer(1, spyServer)
+	timer.ElectionChannel <- 1
+	test.Expect(spyServer.electionStarted).ToBeTrue()
+	timer.ShutDown()
+}
+
+func TestTimer(t *testing.T) {
+	test := quiz.Test(t)
+
+	spyServer := &SpyServer{electionStarted: false, electionCount: 0}
+	timer := NewElectionTimer(0, spyServer)
+
+	timer.StartTimer()
+
+	time.Sleep(1)
+	test.Expect(spyServer.electionStarted).ToBeTrue()
+	timer.ShutDown()
+}
+
+func TestTimerStartsMultipleElections(t *testing.T) {
+	test := quiz.Test(t)
+
+	spyServer := &SpyServer{electionStarted: false, electionCount: 0}
+	timer := NewElectionTimer(2, spyServer)
+
+	timer.StartTimer()
+
+	time.Sleep(100000)
+	test.Expect(spyServer.electionCount).ToBeGreaterThan(1)
+	timer.ShutDown()
+}
