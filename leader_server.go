1a53c16bbab961d64d7a079d86e41ecaa0c0cc2c
William Dix <william.j.dix@gmail.com>
2013-08-28 20:50:23 -0500

    Test <-> Implementation. Oops

--- leader_server.go
+++ leader_server.go
@@ -1,20 +1,15 @@
 package graft
 
-import (
-	"github.com/benmills/quiz"
-	"testing"
-)
+type LeaderServer struct {
+	Voter
+}
 
-func TestGenerateAppendEntriesMessage(t *testing.T) {
-	test := quiz.Test(t)
-
-	server := New()
-
-	message := server.AppendEntries()
-
-	test.Expect(message.Term).ToEqual(0)
-	test.Expect(message.LeaderId).ToEqual(server.Id)
-	test.Expect(message.PrevLogIndex).ToEqual(0)
-	test.Expect(len(message.Entries)).ToEqual(0)
-	test.Expect(message.CommitIndex).ToEqual(0)
+func (server *Server) AppendEntries() AppendEntriesMessage {
+	return AppendEntriesMessage{
+		Term:         server.Term,
+		LeaderId:     server.Id,
+		PrevLogIndex: server.lastLogIndex(),
+		Entries:      []LogEntry{},
+		CommitIndex:  server.lastCommitIndex(),
+	}
 }
