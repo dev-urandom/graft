16bdf4f1e850aa5452440db87c70efe96415e588
William Dix <william.j.dix@gmail.com>
2013-09-13 14:22:19 -0500

    Send a RequestVoteMessage using json over http

--- http_peer.go
+++ http_peer.go
@@ -1,10 +1,17 @@
 package graft
 
+import (
+	"encoding/json"
+	"errors"
+	"github.com/benmills/telephone"
+)
+
 type HttpPeer struct {
+	URL string
 }
 
 func NewHttpPeer(url string) HttpPeer {
-	return HttpPeer{}
+	return HttpPeer{url}
 }
 
 func (peer HttpPeer) ReceiveAppendEntries(message AppendEntriesMessage) AppendEntriesResponseMessage {
@@ -12,5 +19,17 @@ func (peer HttpPeer) ReceiveAppendEntries(message AppendEntriesMessage) AppendEn
 }
 
 func (peer HttpPeer) ReceiveRequestVote(message RequestVoteMessage) (VoteResponseMessage, error) {
-	return VoteResponseMessage{}, nil
+	var voteResponseMessage VoteResponseMessage
+	body, _ := json.Marshal(message)
+	request := telephone.Request{
+		Url:  peer.URL + "/request_vote",
+		Body: string(body),
+	}
+	response := request.Post()
+	if !response.Success {
+		return voteResponseMessage, errors.New("Could Not Communicate With Remote Peer")
+	}
+	json.Unmarshal([]byte(response.ParsedBody), &voteResponseMessage)
+
+	return voteResponseMessage, nil
 }
--- http_peer_test.go
+++ http_peer_test.go
@@ -1,12 +1,53 @@
 package graft
 
 import (
-	/*"github.com/benmills/quiz"*/
+	"encoding/json"
+	"github.com/benmills/quiz"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 )
 
-func TestHttpChannelCanBeAddedToAServersListOfPeers(t *testing.T) {
+func fakeRemoteServer() *httptest.Server {
+	server := http.NewServeMux()
+	server.HandleFunc("/request_vote", func(w http.ResponseWriter, r *http.Request) {
+		var requestVoteMessage RequestVoteMessage
+		rawBody, _ := ioutil.ReadAll(r.Body)
+		json.Unmarshal(rawBody, &requestVoteMessage)
+		response := VoteResponseMessage{
+			Term:        requestVoteMessage.Term,
+			VoteGranted: true,
+		}
+		enc := json.NewEncoder(w)
+		enc.Encode(response)
+	})
+	return httptest.NewServer(server)
+
+}
+
+func TestHttpPeerCanBeAddedToAServersListOfPeers(t *testing.T) {
 	peer := NewHttpPeer("http://localhost:4040")
 	server2 := New()
 	server2.Peers = []Peer{peer}
+}
+
+func TestHttpPeerRequestsVoteOverHttp(t *testing.T) {
+	test := quiz.Test(t)
+	remoteServer := fakeRemoteServer()
+	defer remoteServer.Close()
+
+	peer := NewHttpPeer(remoteServer.URL)
+
+	voteRequest := RequestVoteMessage{
+		Term:         1,
+		CandidateId:  "foo",
+		LastLogIndex: 0,
+		LastLogTerm:  0,
+	}
+
+	response, err := peer.ReceiveRequestVote(voteRequest)
+	test.Expect(err == nil).ToBeTrue()
+	test.Expect(response.Term).ToEqual(1)
+	test.Expect(response.VoteGranted).ToBeTrue()
 }
